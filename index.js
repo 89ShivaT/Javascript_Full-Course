4e167ba056fee1c31137bc904c94c9cb796ad763
Programmer Shiva Rajput <48715128+89ShivaT@users.noreply.github.com>
2025-09-26 13:57:43 +0000

    basic Works

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // console.log("Hello Javascript here ")
+
+// let myStudentname = "Shiva Thakur";
+// var myStudentage = 27;
+// const myStudentgander ="male";
+// console.table([myStudentname,myStudentage,myStudentgander]);
+
+// myStudentname="Karan Rajput";
+// myStudentage=25;
+// // myStudentgander="Female";
+// console.table([myStudentname,myStudentage,myStudentgander]);
+
+// Javascript Data Types primit Data types
+
+// String
+// Number
+// Boolean
+// null
+// undefined
+// BigInt
+// Symbol
+
+// let sTd = "Shiva Thakur";  // String
+// let std = 20;               // Number
+// let std = true;            //Boolean
+// let std = null;              //null
+// let std = undefined;         //undefined           
+
+
+// console.log(typeof(std),": ", std);
+
+let chacker = "Right";
+
+console.log(chacker);
